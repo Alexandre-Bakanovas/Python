2a47df81b3fd197440b2a2df43845ccb38fed4a5
Alexandre-Bakanovas <alexandremazarinbakanovas@gmail.com>
2023-09-11 15:29:58 -0300

    Commit

--- aula97.py
+++ aula97.py
@@ -13,3 +13,16 @@ digito = 9 # Se ele for maior que 9, ele será 0
 # novo_digito = 0 if digito > 9 else digito
 # print(novo_digito)
 
+
+a = 0
+b = 3
+c = 0
+d = 0
+e = 0
+for i in range(50, 20, -2):
+    a = (i/2) + 3
+    b = b + i
+    b = b + 10
+    c = a + 13
+    d = (i - 1) * 2 * 13
+    print(a,b,c,d)
